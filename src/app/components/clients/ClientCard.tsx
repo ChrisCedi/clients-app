253becc2625a5174f935cfc349fb37175299ece9
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-04-25 14:53:59 -0600

    clientDetail and router push

--- src/app/components/clients/ClientCard.tsx
+++ src/app/components/clients/ClientCard.tsx
@@ -1,23 +1,29 @@
 "use client";
 import { Client } from "@/types/clients";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 interface ClientCardProps {
   client: Client;
 }
 
 const ClientCard: React.FC<ClientCardProps> = ({ client }) => {
+  const router = useRouter();
+
   return (
     <motion.div
+      onClick={() => {
+        router.push(`/clientDetail/${client.id}`);
+      }}
       whileHover={{ scale: 1.0 }}
       whileTap={{ scale: 0.9 }}
       className="grid grid-cols-1 h-fit md:grid-cols-2 border-solid border-2 py-11 my-8 rounded-md hover:cursor-pointer"
     >
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-lg md:text-2xl font-bold">
           {client.firstName} {client.lastName}
         </h1>
-        <h1 className="text-xl pb-6">{client.email}</h1>
+        <h1 className="text-md md:text-xl pb-6">{client.email}</h1>
       </div>
       <div className="flex justify-center">
         <img src={client.avatar} alt="avatar" className="h-40 w-40" />
